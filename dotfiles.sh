c0d90438664b0da4d88274b49b7f79c9cae92937
epistrephein <epistrephein@gmail.com>
2015-07-01 18:27:26 +0200

    fixed rdark theme download

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,5 +3,5 @@
 curl -sL https://raw.githubusercontent.com/epistrephein/serverscripts/master/dotfiles/.bashrc >> $HOME/.bashrc
 curl -sL https://raw.githubusercontent.com/epistrephein/serverscripts/master/dotfiles/.inputrc >> $HOME/.inputrc 
 mkdir -p $HOME/.vim/colors
-curl -sL -o $HOME/.vim/colors/rdark-terminal.vim https://raw.githubusercontent.com/epistrephein/serverscripts/master/dotfiles/rdark-terminal.vim
+curl -sL -o $HOME/.vim/colors/rdark.vim https://raw.githubusercontent.com/epistrephein/serverscripts/master/dotfiles/rdark.vim
 curl -sL https://raw.githubusercontent.com/epistrephein/serverscripts/master/dotfiles/.vimrc > $HOME/.vimrc
