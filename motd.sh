64e2441f5449d12f3ab61c71cf5fb870de79718b
epistrephein <epistrephein@gmail.com>
2015-07-01 18:44:02 +0200

    fixed banner download position

--- motd.sh
+++ motd.sh
@@ -34,7 +34,7 @@ fi
 
 # add banner file
 if [ ! -f /etc/update-motd.d/20-banner ]; then
-  wget -q https://raw.githubusercontent.com/epistrephein/serverscripts/master/motd/20-banner
+  wget -q https://raw.githubusercontent.com/epistrephein/serverscripts/master/motd/20-banner -O /etc/update-motd.d/20-banner
   chmod +x /etc/update-motd.d/20-banner
   echo "Adding banner (remember to customize the file /etc/update-motd.d/20-banner)"
 fi
