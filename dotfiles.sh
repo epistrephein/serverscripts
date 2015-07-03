e4bebf9c1daf7bd0969d5a0ac18280dbefdd983d
epistrephein <epistrephein@gmail.com>
2015-07-03 19:39:43 +0200

    more verbose

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 curl -sL https://raw.githubusercontent.com/epistrephein/serverscripts/master/dotfiles/.bashrc >> $HOME/.bashrc
+echo "Updated .bashrc"
+
 curl -sL https://raw.githubusercontent.com/epistrephein/serverscripts/master/dotfiles/.inputrc >> $HOME/.inputrc 
-mkdir -p $HOME/.vim/colors
+echo "Updated .inputrc"
+
+[ ! -d $HOME/.vim/colors ] && mkdir -p $HOME/.vim/colors
 curl -sL -o $HOME/.vim/colors/rdark.vim https://raw.githubusercontent.com/epistrephein/serverscripts/master/dotfiles/rdark.vim
+echo "Installed rdark vim theme"
+
 curl -sL https://raw.githubusercontent.com/epistrephein/serverscripts/master/dotfiles/.vimrc > $HOME/.vimrc
+echo "Updated .vimrc"
 
 # autoremove script
 [ -f $0 ] && rm -- "$0"
