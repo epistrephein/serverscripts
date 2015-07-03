921955e41b1d4608f3be46776f92fe4fe811c0eb
epistrephein <epistrephein@gmail.com>
2015-07-03 14:33:00 +0200

    better sed regex for sshd elements

--- initialserversetup.sh
+++ initialserversetup.sh
@@ -95,15 +95,15 @@ if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
   until [[ ! -z "$SSHPORT" && $SSHPORT =~ $VALIDINPUT ]]; do
     read -p "Invalid input. Please insert a number: " SSHPORT
   done
-  sed -i "s/Port 22/Port $SSHPORT/g" /etc/ssh/sshd_config
+  sed -i "s/^Port/s/^.*$/Port $SSHPORT/g" /etc/ssh/sshd_config
   SSHRESTART=1
 fi
 
 # change ssh login policy
 read -p "Prevent root login and password authentication? [Y/n] " -s -n 1 -r; echo
 if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
-  sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-  sed -i 's/\#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+  sed -i 's/^PermitRootLogin/s/^.*$/PermitRootLogin no/g' /etc/ssh/sshd_config
+  sed -i 's/^\#PasswordAuthentication/s/^.*$/PasswordAuthentication no/g' /etc/ssh/sshd_config
   SSHRESTART=1
 fi
 
